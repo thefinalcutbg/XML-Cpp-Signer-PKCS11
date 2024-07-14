25b6b29a396b8b7d51108aa3bbdd75114e1a3921
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-07-14 15:35:36 +0300

    sign part of XML added

--- src/signer.cpp
+++ src/signer.cpp
@@ -4,141 +4,150 @@
 #include "pkcs11.h"
 
 
-std::string Signer::signEnveloped(const std::string& xml, evp_pkey_st* pkey, x509_st* cert, bool XAdES)
+
+
+std::string Signer::getSignature(const std::string& xml, evp_pkey_st* pkey, x509_st* cert, const std::string& URI, bool XAdES)
 {
 	std::string xadesNode;
-	
-	if(XAdES){
+
+	if (XAdES) {
 		xadesNode =
 			"<xades:SignedProperties Id=\"xadesNode\">"
-					"<xades:SignedSignatureProperties>"
-						"<xades:SigningTime>" 
-						+ FreeFn::get8601timestamp() +
-						"</xades:SigningTime>"
-						"<xades:SigningCertificateV2>"
-							"<xades:Cert>"
-								"<xades:CertDigest>"
-									"<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmlenc#sha256\"/>"
-									"<DigestValue>"
-										+
-											FreeFn::getSHA256DigestBase64(cert)
-										+
-								"</DigestValue>"
-								"</xades:CertDigest>"
-							"</xades:Cert>"
-						"</xades:SigningCertificateV2>"
-						"<xades:SignatureProductionPlaceV2>"
-							"<xades:City/>"
-							"<xades:StateOrProvince/>"
-							"<xades:PostalCode/>"
-							"<xades:CountryName>"
-								+
-									FreeFn::get_country_from_x509(cert)
-								+ 
-							"</xades:CountryName>"
-						"</xades:SignatureProductionPlaceV2>"
-						"<xades:SignerRoleV2>"
-							"<xades:ClaimedRoles>"
-								"<xades:ClaimedRole>SIGNED BY</xades:ClaimedRole>"
-							"</xades:ClaimedRoles>"
-						"</xades:SignerRoleV2>"
-					"</xades:SignedSignatureProperties>"
-					"<xades:SignedDataObjectProperties>"
-						"<xades:DataObjectFormat ObjectReference=\"#r-id-1\">"
-							"<xades:MimeType>text/xml</xades:MimeType>"
-						"</xades:DataObjectFormat>"
-					"</xades:SignedDataObjectProperties>"
-				"</xades:SignedProperties>";
+			"<xades:SignedSignatureProperties>"
+			"<xades:SigningTime>"
+			+ FreeFn::get8601timestamp() +
+			"</xades:SigningTime>"
+			"<xades:SigningCertificateV2>"
+			"<xades:Cert>"
+			"<xades:CertDigest>"
+			"<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmlenc#sha256\"/>"
+			"<DigestValue>"
+			+
+			FreeFn::getSHA256DigestBase64(cert)
+			+
+			"</DigestValue>"
+			"</xades:CertDigest>"
+			"</xades:Cert>"
+			"</xades:SigningCertificateV2>"
+			"<xades:SignatureProductionPlaceV2>"
+			"<xades:City/>"
+			"<xades:StateOrProvince/>"
+			"<xades:PostalCode/>"
+			"<xades:CountryName>"
+			+
+			FreeFn::get_country_from_x509(cert)
+			+
+			"</xades:CountryName>"
+			"</xades:SignatureProductionPlaceV2>"
+			"<xades:SignerRoleV2>"
+			"<xades:ClaimedRoles>"
+			"<xades:ClaimedRole>SIGNED BY</xades:ClaimedRole>"
+			"</xades:ClaimedRoles>"
+			"</xades:SignerRoleV2>"
+			"</xades:SignedSignatureProperties>"
+			"<xades:SignedDataObjectProperties>"
+			"<xades:DataObjectFormat ObjectReference=\"#r-id-1\">"
+			"<xades:MimeType>text/xml</xades:MimeType>"
+			"</xades:DataObjectFormat>"
+			"</xades:SignedDataObjectProperties>"
+			"</xades:SignedProperties>";
 	}
 
 	const std::string signatureNs = "http://www.w3.org/2000/09/xmldsig#";
 
 	std::string signedInfo =
-    "<SignedInfo>"
+		"<SignedInfo>"
 		"<CanonicalizationMethod Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>"
 		"<SignatureMethod Algorithm=\"http://www.w3.org/2001/04/xmldsig-more#rsa-sha256\"/>"
-		"<Reference Id=\"r-id-1\" URI=\"\">"
-			"<Transforms>"
-				"<Transform Algorithm=\"http://www.w3.org/2000/09/xmldsig#enveloped-signature\"/>"
-				"<Transform Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>"
-			"</Transforms>"
-			"<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmlenc#sha256\"/>"
-			"<DigestValue>"
-				//digest value of the document
-                +   
-                FreeFn::base64Encode(
-                    FreeFn::calculateSHA256Digest(
-                        FreeFn::canonicalizeXML(xml)
-                    )
-                )
-                +
-          "</DigestValue>"
-		    "</Reference>"
+		"<Reference Id=\"r-id-1\" URI=\"" + URI +"\">"
+		"<Transforms>"
+		"<Transform Algorithm=\"http://www.w3.org/2000/09/xmldsig#enveloped-signature\"/>"
+		"<Transform Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>"
+		"</Transforms>"
+		"<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmlenc#sha256\"/>"
+		"<DigestValue>"
+		//digest value of the document
+		+
+		FreeFn::base64Encode(
+			FreeFn::calculateSHA256Digest(
+				FreeFn::canonicalizeXML(xml)
+			)
+		)
+		+
+		"</DigestValue>"
+		"</Reference>"
 		;
 
 	//insert the XAdES ref
-	if(XAdES){
+	if (XAdES) {
 
 		signedInfo +=
 
-		"<Reference Type=\"http://uri.etsi.org/01903#SignedProperties\" URI=\"#xadesNode\">"
-			    "<Transforms>"
-				    "<Transform Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>"
-			    "</Transforms>"
-			    "<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmlenc#sha256\"/>"
-			    "<DigestValue>" 
-                   //digest value of the Signed properties
-				+
-					FreeFn::base64Encode(
-						FreeFn::calculateSHA256Digest(
-							FreeFn::canonicalizeXML(
-								FreeFn::addNamespacesToRoot(xadesNode, { {"xades", "http://uri.etsi.org/01903/v1.3.2#"}, {"", signatureNs } })
-							)
-						)
+			"<Reference Type=\"http://uri.etsi.org/01903#SignedProperties\" URI=\"#xadesNode\">"
+			"<Transforms>"
+			"<Transform Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>"
+			"</Transforms>"
+			"<DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmlenc#sha256\"/>"
+			"<DigestValue>"
+			//digest value of the Signed properties
+			+
+			FreeFn::base64Encode(
+				FreeFn::calculateSHA256Digest(
+					FreeFn::canonicalizeXML(
+						FreeFn::addNamespacesToRoot(xadesNode, { {"xades", "http://uri.etsi.org/01903/v1.3.2#"}, {"", signatureNs } })
 					)
-				+
+				)
+			)
+			+
 			"</DigestValue>"
-		"</Reference>"
-		;
+			"</Reference>"
+			;
 	}
 
 	signedInfo += "</SignedInfo>";
 
-	
-	std::string signature = 
-		"<Signature xmlns=\""+ signatureNs + "\">" +
-			signedInfo +
-			"<SignatureValue>"
-			+
-				FreeFn::calculateSignature(
-						FreeFn::canonicalizeXML(
-							FreeFn::addNamespacesToRoot( //since we use exclusive C14, only the signatureNs is required
-								signedInfo, NSList{ { "", signatureNs} }
-							)
-					), pkey
+
+	std::string signature =
+		"<Signature xmlns=\"" + signatureNs + "\">" +
+		signedInfo +
+		"<SignatureValue>"
+		+
+		FreeFn::calculateSignature(
+			FreeFn::canonicalizeXML(
+				FreeFn::addNamespacesToRoot( //since we use exclusive C14, only the signatureNs is required
+					signedInfo, NSList{ { "", signatureNs} }
 				)
-			 +
-			"</SignatureValue>" +
-			"<KeyInfo><X509Data><X509Certificate>" +
-				FreeFn::base64Encode(cert) +
-			"</X509Certificate></X509Data></KeyInfo>"
+			), pkey
+		)
+		+
+		"</SignatureValue>" +
+		"<KeyInfo><X509Data><X509Certificate>" +
+		FreeFn::base64Encode(cert) +
+		"</X509Certificate></X509Data></KeyInfo>"
 		;
-	
-	if(XAdES){		
+
+	if (XAdES) {
 		//insert XAdES object
-		signature += 
+		signature +=
 
 			"<Object>"
-		"<xades:QualifyingProperties xmlns:xades=\"http://uri.etsi.org/01903/v1.3.2#\">" +
-					xadesNode +
-				"</xades:QualifyingProperties>"
+			"<xades:QualifyingProperties xmlns:xades=\"http://uri.etsi.org/01903/v1.3.2#\">" +
+			xadesNode +
+			"</xades:QualifyingProperties>"
 			"</Object>"
-		;
+			;
 	}
-	
+
 	signature += "</Signature>";
 
-	auto result = xml;
+	return signature;
+}
+
+std::string Signer::signEnveloped(const std::string& xml, evp_pkey_st* pkey, x509_st* cert, bool XAdES)
+{
+	auto signature = getSignature(xml, pkey, cert, std::string(), XAdES);
+
+		auto result = xml;
 
 	int insertPosition = result.size() - 1;
 
--- src/signer.h
+++ src/signer.h
@@ -7,4 +7,5 @@ struct evp_pkey_st;
 namespace Signer
 {
 	std::string signEnveloped(const std::string& xml, evp_pkey_st* pkey, x509_st* cert, bool XAdES = false);
+	std::string getSignature(const std::string& xml, evp_pkey_st* pkey, x509_st* cert, const std::string& URI = "", bool XAdES = false);
 }
