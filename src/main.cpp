e5687513fe289df73545798f20e1af49ef9b4631
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-07-15 11:10:41 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 int main()
 {
-    std::string xml = R"xml(<?xml version="1.0"?><example>some data to sign</example>)xml";
+    std::string xml = R"xml(<?xml version="1.0"?><Example>some data to sign</Example>)xml";
 
     PKCS11::setMiddlewareFilePath({ "C:/Program Files/OpenSC Project/OpenSC/pkcs11/opensc-pkcs11.dll" });
 
@@ -37,7 +37,7 @@ int main()
 
     //Sign SOAP message body
 
-    std::string body = R"(<e:Body id="signedContent"><ns3:query xmlns:ns1="http://pis.technologica.com/views/" xmlns:ns3="http://pis.technologica.com/ws/"><ns3:user><ns3:egn>8903261129</ns3:egn></ns3:user><ns3:from_clause>INYEAR_DENTAL_ACTS</ns3:from_clause><ns3:orderby_clause><ns1:ocolumn sort="DESC">ACTIVITY_DATE</ns1:ocolumn></ns3:orderby_clause></ns3:query></e:Body>)";
+    std::string body = R"(<e:Body id="signedContent"><Example>some data to sign</Example></e:Body>)";
 
     std::string signedSoap = R"(<?xml version="1.0" encoding="utf-8"?><e:Envelope xmlns:e="http://schemas.xmlsoap.org/soap/envelope/"><e:Header>)";
 
